212f61dbc39c171cb5a606772204daa91c9ef72a
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-12-21 22:51:42 +0100

    feat: implement TextMother class for generating random text values

--- object_mother_pattern/mothers/__init__.py
+++ object_mother_pattern/mothers/__init__.py
@@ -1,6 +1,9 @@
-from .primitives import BoolMother, IntegerMother
+from .primitives import BoolMother, IntegerMother, StringMother
+from .text_mother import TextMother
 
 __all__ = (
     'BoolMother',
     'IntegerMother',
+    'StringMother',
+    'TextMother',
 )

a1061ff82f342a736c5dce5b70678bb9a0dd09c7
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-12-21 20:11:39 +0100

    ✨ feat: implement StringMother class for generating random string values

--- object_mother_pattern/mothers/primitives/__init__.py
+++ object_mother_pattern/mothers/primitives/__init__.py
@@ -1,7 +1,9 @@
 from .bool_mother import BoolMother
 from .integer_mother import IntegerMother
+from .string_mother import StringMother
 
 __all__ = (
     'BoolMother',
     'IntegerMother',
+    'StringMother',
 )

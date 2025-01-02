7a9504edadf36b6fe653217fcadbd699978a87f2
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-01-02 09:46:51 +0100

    feat: implement BytesMother

--- object_mother_pattern/mothers/__init__.py
+++ object_mother_pattern/mothers/__init__.py
@@ -1,9 +1,11 @@
 from .name_mother import NameMother
-from .primitives import BoolMother, IntegerMother, StringMother
+from .primitives import BoolMother, BytesMother, FloatMother, IntegerMother, StringMother
 from .text_mother import TextMother
 
 __all__ = (
     'BoolMother',
+    'BytesMother',
+    'FloatMother',
     'IntegerMother',
     'NameMother',
     'StringMother',
--- object_mother_pattern/mothers/primitives/__init__.py
+++ object_mother_pattern/mothers/primitives/__init__.py
@@ -1,10 +1,12 @@
 from .bool_mother import BoolMother
+from .bytes_mother import BytesMother
 from .float_mother import FloatMother
 from .integer_mother import IntegerMother
 from .string_mother import StringMother
 
 __all__ = (
     'BoolMother',
+    'BytesMother',
     'FloatMother',
     'IntegerMother',
     'StringMother',

ea9c711291ecf6c8d41c4fa16b7371af6a8144ae
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-05-12 08:59:39 +0200

    feat(people): implement password mother
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- object_mother_pattern/mothers/primitives/utils/alphabets.py
+++ object_mother_pattern/mothers/primitives/utils/alphabets.py
@@ -2,6 +2,9 @@
 This module describes several alphabets.
 """
 
+SPECIAL_CHARS = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'  # some chars can break csv files (, ; |)
+SUPER_SPECIAL_CHARS = ' £çÇñÑ¡¿'  # some chars are not accepted by some websites (contains space)
+
 ALPHABET_LOWERCASE_BASIC = 'abcdefghijklmnopqrstuvwxyz'
 ALPHABET_UPPERCASE_BASIC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 DIGITS_BASIC = '0123456789'

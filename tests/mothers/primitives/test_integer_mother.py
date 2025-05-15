cb7b433fee0e537d9796baacb5d9112f8c96b4cd
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-05-15 21:43:35 +0200

    test: improveIntegerMother tests reliability

--- tests/mothers/primitives/test_integer_mother.py
+++ tests/mothers/primitives/test_integer_mother.py
@@ -8,9 +8,9 @@ from object_mother_pattern.mothers import IntegerMother
 
 
 @mark.unit_testing
-def test_integer_mother_happy_path() -> None:
+def test_integer_mother_create_method_happy_path() -> None:
     """
-    Test IntegerMother happy path.
+    Check that IntegerMother create method returns a random integer between -100 and 100.
     """
     value = IntegerMother.create()
 
@@ -19,9 +19,9 @@ def test_integer_mother_happy_path() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_value() -> None:
+def test_integer_mother_create_method_value() -> None:
     """
-    Test IntegerMother create method with value.
+    Check that IntegerMother create method returns the provided value.
     """
     value = IntegerMother.create()
 
@@ -29,27 +29,9 @@ def test_integer_mother_value() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_invalid_type() -> None:
+def test_integer_mother_create_method_invalid_value_type() -> None:
     """
-    Test IntegerMother create method with invalid type.
-    """
-    assert type(IntegerMother.invalid_type()) is not int
-
-
-@mark.unit_testing
-def test_integer_mother_same_min_max() -> None:
-    """
-    Test IntegerMother create method with same min and max.
-    """
-    value = IntegerMother.create()
-
-    assert IntegerMother.create(min=value, max=value) == value
-
-
-@mark.unit_testing
-def test_integer_mother_invalid_value_type() -> None:
-    """
-    Test IntegerMother create method with invalid value type.
+    Check that IntegerMother create method raises a TypeError when the provided value is not an integer.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -59,21 +41,19 @@ def test_integer_mother_invalid_value_type() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_invalid_min_type() -> None:
+def test_integer_mother_create_method_same_min_max() -> None:
     """
-    Test IntegerMother create method with invalid min type.
+    Check that IntegerMother create method returns the provided value when min and max are the same.
     """
-    with assert_raises(
-        expected_exception=TypeError,
-        match='IntegerMother min value must be an integer.',
-    ):
-        IntegerMother.create(min=IntegerMother.invalid_type())
+    value = IntegerMother.create()
+
+    assert IntegerMother.create(min=value, max=value) == value
 
 
 @mark.unit_testing
-def test_integer_mother_invalid_max_type() -> None:
+def test_integer_mother_create_method_invalid_max_type() -> None:
     """
-    Test IntegerMother create method with invalid max type.
+    Check that IntegerMother create method raises a TypeError when the provided max is not an integer.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -83,9 +63,21 @@ def test_integer_mother_invalid_max_type() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_min_greater_than_max() -> None:
+def test_integer_mother_create_method_invalid_min_type() -> None:
     """
-    Test IntegerMother create method with min greater than max.
+    Check that IntegerMother create method raises a TypeError when the provided min is not an integer.
+    """
+    with assert_raises(
+        expected_exception=TypeError,
+        match='IntegerMother min value must be an integer.',
+    ):
+        IntegerMother.create(min=IntegerMother.invalid_type())
+
+
+@mark.unit_testing
+def test_integer_mother_create_method_min_greater_than_max() -> None:
+    """
+    Check that IntegerMother create method raises a ValueError when the provided min is greater than max.
     """
     min_value = IntegerMother.negative()
     max_value = IntegerMother.positive()
@@ -98,20 +90,20 @@ def test_integer_mother_min_greater_than_max() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_positive() -> None:
+def test_integer_mother_positive_method_happy_path() -> None:
     """
-    Test IntegerMother positive method.
+    Check that IntegerMother positive method returns a positive integer.
     """
     value = IntegerMother.positive()
 
     assert type(value) is int
-    assert value > 0
+    assert 0 < value <= 100
 
 
 @mark.unit_testing
-def test_integer_mother_positive_invalid_max_type() -> None:
+def test_integer_mother_positive_method_invalid_max_type() -> None:
     """
-    Test IntegerMother positive method with invalid max type.
+    Check that IntegerMother positive method raises a TypeError when the provided max is not an integer.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -121,11 +113,41 @@ def test_integer_mother_positive_invalid_max_type() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_positive_max_less_than_one() -> None:
+def test_integer_mother_positive_method_max_value() -> None:
     """
-    Test IntegerMother positive method with max less than 1.
+    Check that IntegerMother positive method max parameter is the maximum permitted value.
     """
-    max_value = IntegerMother.create(max=0)
+    IntegerMother.positive(max=1)
+
+
+@mark.unit_testing
+def test_integer_mother_positive_method_max_random_value() -> None:
+    """
+    Check that IntegerMother positive method max parameter is a random positive integer.
+    """
+    max_value = IntegerMother.positive()
+
+    IntegerMother.positive(max=max_value)
+
+
+@mark.unit_testing
+def test_integer_mother_positive_method_max_not_greater_than_zero_value() -> None:
+    """
+    Check that IntegerMother positive method raises a ValueError when the provided max is not greater than 0.
+    """
+    with assert_raises(
+        expected_exception=ValueError,
+        match='IntegerMother min value must be less than or equal to max value.',
+    ):
+        IntegerMother.positive(max=0)
+
+
+@mark.unit_testing
+def test_integer_mother_positive_method_max_not_greater_than_zero_random_value() -> None:
+    """
+    Check that IntegerMother positive method raises a ValueError when the provided max is not greater than 0.
+    """
+    max_value = IntegerMother.negative_or_zero()
 
     with assert_raises(
         expected_exception=ValueError,
@@ -135,20 +157,31 @@ def test_integer_mother_positive_max_less_than_one() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_negative() -> None:
+def test_integer_mother_positive_or_zero_method_happy_path() -> None:
+    """
+    Check that IntegerMother positive_or_zero method returns a random integer between 0 and 100.
+    """
+    value = IntegerMother.positive_or_zero()
+
+    assert type(value) is int
+    assert 0 <= value <= 100
+
+
+@mark.unit_testing
+def test_integer_mother_positive_or_zero_method_invalid_max_type() -> None:
     """
     Test IntegerMother negative method.
     """
     value = IntegerMother.negative()
 
     assert type(value) is int
-    assert value < 0
+    assert -100 <= value < 0
 
 
 @mark.unit_testing
-def test_integer_mother_negative_invalid_min_type() -> None:
+def test_integer_mother_negative_method_invalid_min_type() -> None:
     """
-    Test IntegerMother negative method with invalid min type.
+    Check that IntegerMother negative method raises a TypeError when the provided min is not an integer.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -158,11 +191,29 @@ def test_integer_mother_negative_invalid_min_type() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_negative_min_greater_than_zero() -> None:
+def test_integer_mother_negative_method_min_value() -> None:
     """
-    Test IntegerMother negative method with min greater than 0.
+    Check that IntegerMother negative method min parameter is the minimum permitted value.
     """
-    min_value = IntegerMother.create(min=1)
+    IntegerMother.negative(min=-1)
+
+
+@mark.unit_testing
+def test_integer_mother_negative_method_min_random_value() -> None:
+    """
+    Check that IntegerMother negative method min parameter is a random negative integer.
+    """
+    min_value = IntegerMother.negative()
+
+    IntegerMother.negative(min=min_value)
+
+
+@mark.unit_testing
+def test_integer_mother_negative_method_min_greater_than_zero() -> None:
+    """
+    Check that IntegerMother negative method raises a ValueError when the provided min is greater than or equal to 0.
+    """
+    min_value = IntegerMother.positive_or_zero()
 
     with assert_raises(
         expected_exception=ValueError,
@@ -172,25 +223,66 @@ def test_integer_mother_negative_min_greater_than_zero() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_out_of_range() -> None:
+def test_integer_mother_negative_or_zero_method_happy_path() -> None:
     """
-    Test IntegerMother out_of_range method.
+    Check that IntegerMother negative_or_zero method returns a random integer between -100 and 0.
     """
-    min_value = IntegerMother.create(max=0)
-    max_value = IntegerMother.create(min=0)
-    range_value = IntegerMother.create(min=0)
-
-    value = IntegerMother.out_of_range(min=min_value, max=max_value, range=range_value)
+    value = IntegerMother.negative_or_zero()
 
     assert type(value) is int
-    assert value < min_value or value > max_value
-    assert (min_value - range_value <= value <= min_value) or (max_value <= value <= max_value + range_value)
+    assert -100 <= value <= 0
 
 
 @mark.unit_testing
-def test_integer_mother_out_of_range_invalid_min_type() -> None:
+def test_integer_mother_negative_or_zero_method_invalid_min_type() -> None:
     """
-    Test IntegerMother out_of_range method with invalid min type.
+    Check that IntegerMother negative_or_zero method raises a TypeError when the provided min is not an integer.
+    """
+    with assert_raises(
+        expected_exception=TypeError,
+        match='IntegerMother min value must be an integer.',
+    ):
+        IntegerMother.negative_or_zero(min=IntegerMother.invalid_type())
+
+
+@mark.unit_testing
+def test_integer_mother_negative_or_zero_method_min_value() -> None:
+    """
+    Check that IntegerMother negative_or_zero method min parameter is the minimum permitted value.
+    """
+    IntegerMother.negative_or_zero(min=-1)
+
+
+@mark.unit_testing
+def test_integer_mother_negative_or_zero_method_min_random_value() -> None:
+    """
+    Check that IntegerMother negative_or_zero method min parameter is a random negative integer.
+    """
+    min_value = IntegerMother.negative_or_zero()
+
+    IntegerMother.negative_or_zero(min=min_value)
+
+
+@mark.unit_testing
+def test_integer_mother_out_of_range_method_happy_path() -> None:
+    """
+    Check that IntegerMother out_of_range method returns a random integer value that is either less than min_value or
+    greater than max_value.
+    """
+    min_value = IntegerMother.negative()
+    max_value = IntegerMother.positive()
+
+    value = IntegerMother.out_of_range(min=min_value, max=max_value)
+
+    assert type(value) is int
+    assert value < min_value or value > max_value
+    assert value != min_value and value != max_value
+
+
+@mark.unit_testing
+def test_integer_mother_out_of_range_method_invalid_min_type() -> None:
+    """
+    Check that IntegerMother out_of_range method raises a TypeError when the provided min is not an integer.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -200,9 +292,9 @@ def test_integer_mother_out_of_range_invalid_min_type() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_out_of_range_invalid_max_type() -> None:
+def test_integer_mother_out_of_range_method_invalid_max_type() -> None:
     """
-    Test IntegerMother out_of_range method with invalid max type.
+    Check that IntegerMother out_of_range method raises a TypeError when the provided max is not an integer.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -212,9 +304,9 @@ def test_integer_mother_out_of_range_invalid_max_type() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_out_of_range_min_greater_than_max() -> None:
+def test_integer_mother_out_of_range_method_min_greater_than_max() -> None:
     """
-    Test IntegerMother out_of_range method with min greater than max.
+    Check that IntegerMother out_of_range method raises a ValueError when the provided min is greater than max.
     """
     min_value = IntegerMother.positive()
     max_value = IntegerMother.negative()
@@ -227,9 +319,9 @@ def test_integer_mother_out_of_range_min_greater_than_max() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_out_of_range_invalid_range_type() -> None:
+def test_integer_mother_out_of_range_method_invalid_range_type() -> None:
     """
-    Test IntegerMother out_of_range method with invalid range type.
+    Check that IntegerMother out_of_range method raises a TypeError when the provided range is not an integer.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -239,12 +331,33 @@ def test_integer_mother_out_of_range_invalid_range_type() -> None:
 
 
 @mark.unit_testing
-def test_integer_mother_out_of_range_negative_range() -> None:
+def test_integer_mother_out_of_range_method_negative_range_value() -> None:
     """
-    Test IntegerMother out_of_range method with negative range.
+    Check that IntegerMother out_of_range method raises a ValueError when the provided range is the minimum permitted
+    value.
     """
     with assert_raises(
         expected_exception=ValueError,
-        match='IntegerMother range must be a positive integer.',
+        match='IntegerMother range must be greater than 0.',
     ):
-        IntegerMother.out_of_range(range=IntegerMother.negative())
+        IntegerMother.out_of_range(range=0)
+
+
+@mark.unit_testing
+def test_integer_mother_out_of_range_method_negative_random_range_value() -> None:
+    """
+    Check that IntegerMother out_of_range method raises a ValueError when the provided range is a random negative value.
+    """
+    with assert_raises(
+        expected_exception=ValueError,
+        match='IntegerMother range must be greater than 0.',
+    ):
+        IntegerMother.out_of_range(range=IntegerMother.negative_or_zero())
+
+
+@mark.unit_testing
+def test_integer_mother_create_method_invalid_type() -> None:
+    """
+    Check that IntegerMother create method raises a TypeError when the provided value is not an integer.
+    """
+    assert type(IntegerMother.invalid_type()) is not int

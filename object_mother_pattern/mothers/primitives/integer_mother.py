a5a7eb64c0b26e83418c31111740e604e539deac
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-05-15 21:42:39 +0200

    feat: implement positive_or_zero and negative_or_zero methods in IntegerMother

--- object_mother_pattern/mothers/primitives/integer_mother.py
+++ object_mother_pattern/mothers/primitives/integer_mother.py
@@ -81,10 +81,10 @@ class IntegerMother(BaseMother[int]):
     @classmethod
     def positive(cls, *, max: int = 100) -> int:
         """
-        Create a random positive integer with an upper bound of `max`.
+        Create a random positive integer with an upper bound of `max`, from 1 to `max` (both included).
 
         Args:
-            max (int, optional): Upper bound for the positive integer. Must be >= 0. Defaults to 100.
+            max (int, optional): Upper bound for the positive integer. Must be > 0. Defaults to 100.
 
         Raises:
             TypeError: If `max` is not an integer.
@@ -105,9 +105,35 @@ class IntegerMother(BaseMother[int]):
         return cls.create(min=1, max=max)
 
     @classmethod
+    def positive_or_zero(cls, *, max: int = 100) -> int:
+        """
+        Create a random positive integer (including 0) with an upper bound of `max`, from 0 to `max` (both included).
+
+        Args:
+            max (int, optional): Upper bound for the positive integer. Must be >= 0. Defaults to 100.
+
+        Raises:
+            TypeError: If `max` is not an integer.
+            ValueError: If `max` is not greater than or equal to 0.
+
+        Returns:
+            int: A randomly positive integer value.
+
+        Example:
+        ```python
+        from object_mother_pattern.mothers import IntegerMother
+
+        positive = IntegerMother.positive_or_zero(max=15)
+        print(positive)
+        # >>> 2
+        ```
+        """
+        return cls.create(min=0, max=max)
+
+    @classmethod
     def negative(cls, *, min: int = -100) -> int:
         """
-        Create a random negative integer with a lower bound of `min`.
+        Create a random negative integer with a lower bound of `min`, from `min` to -1 (both included).
 
         Args:
             min (int, optional): Lower bound for the negative integer. Must be < 0. Defaults to -100.
@@ -131,22 +157,48 @@ class IntegerMother(BaseMother[int]):
         return cls.create(min=min, max=-1)
 
     @classmethod
+    def negative_or_zero(cls, *, min: int = -100) -> int:
+        """
+        Create a random negative integer (including 0) with a lower bound of `min`, from `min` to 0 (both included).
+
+        Args:
+            min (int, optional): Lower bound for the negative integer. Must be < 0. Defaults to -100.
+
+        Raises:
+            TypeError: If `min` is not an integer.
+            ValueError: If `min` is not less than 0.
+
+        Returns:
+            int: A randomly negative integer value.
+
+        Example:
+        ```python
+        from object_mother_pattern.mothers import IntegerMother
+
+        negative = IntegerMother.negative_or_zero(min=-61)
+        print(negative)
+        # >>> -45
+        ```
+        """
+        return cls.create(min=min, max=0)
+
+    @classmethod
     def out_of_range(cls, *, min: int = -100, max: int = 100, range: int = 100) -> int:
         """
-        Create a random integer value that is either less than `min` or greater than `max` by a time offset
+        Create a random integer value that is either less than `min` or greater than `max` (both excluded) by a range
         specified by the `range` parameter.
 
         Args:
             min (int, optional): The lower bound of the range. Defaults to -100.
             max (int, optional): The upper bound of the range. Defaults to 100.
-            range (int, optional): The range offset. Must be >= 0. Defaults to 100.
+            range (int, optional): The range offset. Must be > 0. Defaults to 100.
 
         Raises:
             TypeError: If `min` is not an integer.
             TypeError: If `max` is not an integer.
             ValueError: If `min` is greater than `max`.
             TypeError: If `range` is not an integer.
-            ValueError: If `range` is a negative integer.
+            ValueError: If `range` is a non-positive integer.
 
         Returns:
             int: A randomly generated integer value out of the provided range.
@@ -172,13 +224,12 @@ class IntegerMother(BaseMother[int]):
         if type(range) is not int:
             raise TypeError('IntegerMother range must be an integer.')
 
-        if range < 0:
-            raise ValueError('IntegerMother range must be a positive integer.')
+        if range <= 0:
+            raise ValueError('IntegerMother range must be greater than 0.')
 
-        epsilon = 1
         return choice(  # noqa: S311
             seq=[
-                randint(a=min - range, b=min - epsilon),  # noqa: S311
-                randint(a=max + epsilon, b=max + range),  # noqa: S311
+                randint(a=min - range, b=min - 1),  # noqa: S311
+                randint(a=max + 1, b=max + range),  # noqa: S311
             ]
         )

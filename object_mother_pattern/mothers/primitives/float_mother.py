e0a8be021cd99d9ad56210ffb49d55f0a9a6985a
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-05-16 08:04:53 +0200

    ✨ feat: simplify float mother api
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- object_mother_pattern/mothers/primitives/float_mother.py
+++ object_mother_pattern/mothers/primitives/float_mother.py
@@ -2,7 +2,7 @@
 FloatMother module.
 """
 
-from random import choice, randint, uniform
+from random import randint, uniform
 from sys import version_info
 
 if version_info >= (3, 12):
@@ -49,7 +49,7 @@ class FloatMother(BaseMother[float]):
             value (int | float | None, optional): Specific value to return. Defaults to None.
             min (int | float, optional): Minimum value of the range. Defaults to -1.0.
             max (int | float, optional): Maximum value of the range. Must be >= `min`. Defaults to 1.0.
-            decimals (int | None, optional): Number of decimal places for the float. Must be >= 1 and <= 10. Defaults
+            decimals (int | None, optional): Number of decimal places for the float. Must be >= 0 and <= 10. Defaults
             to None.
 
         Raises:
@@ -103,123 +103,4 @@ class FloatMother(BaseMother[float]):
         if min == max:
             return round(number=min, ndigits=decimals)
 
-        if min == 0:
-            min = 0.0000000001  # pragma: no cover
-
-        if max == 0:
-            max = 0.0000000001  # pragma: no cover
-
         return round(number=uniform(a=min, b=max), ndigits=decimals)  # noqa: S311
-
-    @classmethod
-    def positive(cls, *, max: int | float = 1.0, decimals: int | None = None) -> float:
-        """
-        Create a random positive float with an upper bound of `max` and if provided, rounded to the `decimals` number of
-        decimal places, otherwise a random number of decimal places between 0 and 10.
-
-        Args:
-            max (int | float, optional): Upper bound for the positive float. Must be >= 0. Defaults to 1.0.
-            decimals (int | None, optional): Number of decimal places. Must be >= 0 and <= 10. Defaults to None.
-
-        Raises:
-            TypeError: If `max` is not an integer or a float.
-            ValueError: If `max` is not greater than 0.
-            TypeError: If `decimals` is provided but is not an integer.
-            ValueError: If `decimals` is not between 0 and 10 (inclusive).
-
-        Returns:
-            float: A randomly positive float value rounded to the specified number of decimal places.
-
-        Example:
-        ```python
-        from object_mother_pattern.mothers import FloatMother
-
-        positive = FloatMother.positive(max=15)
-        print(positive)
-        # >>> 8.71
-        ```
-        """
-        return cls.create(min=0.0000000001, max=max, decimals=decimals)
-
-    @classmethod
-    def negative(cls, *, min: int | float = -1.0, decimals: int | None = None) -> float:
-        """
-        Create a random negative float with a lower bound of `min` and if provided, rounded to the `decimals` number of
-        decimal places, otherwise a random number of decimal places between 0 and 10.
-
-        Args:
-            min (int | float, optional): Lower bound for the negative float. Must be < 0. Defaults to -1.0.
-            decimals (int | None, optional): Number of decimal places. Must be >= 0 and <= 10. Defaults to None.
-
-        Raises:
-            TypeError: If `min` is not an integer or a float.
-            ValueError: If `min` is greater than -1.
-            TypeError: If `decimals` is provided but is not an integer.
-            ValueError: If `decimals` is not between 0 and 10 (inclusive).
-
-        Returns:
-            float: A randomly negative float value rounded to the specified number of decimal places.
-
-        Example:
-        ```python
-        from object_mother_pattern.mothers import FloatMother
-
-        negative = FloatMother.negative(min=-61)
-        print(negative)
-        # >>> -13.93
-        ```
-        """
-        return cls.create(min=min, max=-0.0000000001, decimals=decimals)
-
-    @classmethod
-    def out_of_range(cls, *, min: int | float = -1.0, max: int | float = 1.0, range: int | float = 1) -> float:
-        """
-        Create a random float value that is either less than `min` or greater than `max` by an offset
-        specified by the `range` parameter.
-
-        Args:
-            min (int | float, optional): The lower bound of the range. Defaults to -1.0.
-            max (int | float, optional): The upper bound of the range. Defaults to 1.0.
-            range (int | float, optional): The range offset. Must be >= 0. Defaults to 1.
-
-        Raises:
-            TypeError: If `min` is not an integer or a float.
-            TypeError: If `max` is not an integer or a float.
-            ValueError: If `min` is greater than `max`.
-            TypeError: If `range` is not an integer or a float.
-            ValueError: If `range` is a negative value.
-
-        Returns:
-            float: A randomly generated float value out of the provided range.
-
-        Example:
-        ```python
-        from object_mother_pattern.mothers import FloatMother
-
-        number = FloatMother.out_of_range()
-        print(number)
-        # >>> -1.2073998516955866
-        ```
-        """
-        if type(min) is not int and type(min) is not float:
-            raise TypeError('FloatMother min value must be an integer or a float.')
-
-        if type(max) is not int and type(max) is not float:
-            raise TypeError('FloatMother max value must be an integer or a float.')
-
-        if min > max:
-            raise ValueError('FloatMother min value must be less than or equal to max value.')
-
-        if type(range) is not int and type(range) is not float:
-            raise TypeError('FloatMother range must be an integer or a float.')
-
-        if range < 0:
-            raise ValueError('FloatMother range must be a positive value.')
-
-        epsilon = 1e-12
-        return choice(  # noqa: S311
-            seq=[
-                uniform(a=min - range, b=min - epsilon),  # noqa: S311
-                uniform(a=max + epsilon, b=max + range),  # noqa: S311
-            ]
-        )
--- tests/mothers/primitives/test_float_mother.py
+++ tests/mothers/primitives/test_float_mother.py
@@ -8,20 +8,22 @@ from object_mother_pattern.mothers import FloatMother, IntegerMother
 
 
 @mark.unit_testing
-def test_float_mother_happy_path() -> None:
+def test_float_mother_create_method_happy_path() -> None:
     """
-    Test FloatMother happy path.
+    Check that FloatMother create method returns a random float between -1 and 1 with a random number of decimals
+    between 0 and 10.
     """
     value = FloatMother.create()
 
     assert type(value) is float
     assert -1 <= value <= 1
+    assert 0 <= len(str(value).split('.')[1]) <= 10
 
 
 @mark.unit_testing
-def test_float_mother_value() -> None:
+def test_float_mother_create_method_value() -> None:
     """
-    Test FloatMother create method with value.
+    Check that FloatMother create method returns the provided value.
     """
     value = FloatMother.create()
 
@@ -29,27 +31,9 @@ def test_float_mother_value() -> None:
 
 
 @mark.unit_testing
-def test_float_mother_invalid_type() -> None:
+def test_float_mother_create_method_invalid_value_type() -> None:
     """
-    Test FloatMother create method with invalid type.
-    """
-    assert type(FloatMother.invalid_type()) is not float
-
-
-@mark.unit_testing
-def test_float_mother_same_min_max() -> None:
-    """
-    Test FloatMother create method with same min and max.
-    """
-    value = FloatMother.create()
-
-    assert FloatMother.create(min=value, max=value, decimals=10) == value
-
-
-@mark.unit_testing
-def test_float_mother_invalid_value_type() -> None:
-    """
-    Test FloatMother create method with invalid value type.
+    Check that FloatMother create method raises a TypeError when the provided value is not an integer or a float.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -59,9 +43,20 @@ def test_float_mother_invalid_value_type() -> None:
 
 
 @mark.unit_testing
-def test_float_mother_invalid_min_type() -> None:
+def test_float_mother_create_method_same_min_max() -> None:
     """
-    Test FloatMother create method with invalid min type.
+    Check that FloatMother create method returns the provided value when min and max are the same.
+    """
+    decimal_number = IntegerMother.create(min=0, max=10)
+    value = FloatMother.create(decimals=decimal_number)
+
+    assert FloatMother.create(min=value, max=value, decimals=decimal_number) == value
+
+
+@mark.unit_testing
+def test_float_mother_create_method_invalid_min_type() -> None:
+    """
+    Check that FloatMother create method raises a TypeError when the provided min is not an integer or a float.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -71,9 +66,9 @@ def test_float_mother_invalid_min_type() -> None:
 
 
 @mark.unit_testing
-def test_float_mother_invalid_max_type() -> None:
+def test_float_mother_create_method_invalid_max_type() -> None:
     """
-    Test FloatMother create method with invalid max type.
+    Check that FloatMother create method raises a TypeError when the provided max is not an integer or a float.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -83,9 +78,9 @@ def test_float_mother_invalid_max_type() -> None:
 
 
 @mark.unit_testing
-def test_float_mother_invalid_decimals_type() -> None:
+def test_float_mother_create_method_invalid_decimals_type() -> None:
     """
-    Test FloatMother create method with invalid decimals type.
+    Check that FloatMother create method raises a TypeError when the provided decimals is not an integer.
     """
     with assert_raises(
         expected_exception=TypeError,
@@ -95,36 +90,12 @@ def test_float_mother_invalid_decimals_type() -> None:
 
 
 @mark.unit_testing
-def test_float_mother_invalid_decimals_less_than_zero() -> None:
+def test_float_mother_create_method_min_greater_than_max() -> None:
     """
-    Test FloatMother create method with invalid decimals value less than zero.
+    Check that FloatMother create method raises a ValueError when the provided min is greater than max.
     """
-    with assert_raises(
-        expected_exception=ValueError,
-        match='FloatMother decimals value must be greater than or equal to 0.',
-    ):
-        FloatMother.create(decimals=IntegerMother.negative())
-
-
-@mark.unit_testing
-def test_float_mother_invalid_decimals_higher_than_ten() -> None:
-    """
-    Test FloatMother create method with invalid decimals value higher than ten.
-    """
-    with assert_raises(
-        expected_exception=ValueError,
-        match='FloatMother decimals value must be less than or equal to 10.',
-    ):
-        FloatMother.create(decimals=IntegerMother.create(min=11))
-
-
-@mark.unit_testing
-def test_float_mother_min_greater_than_max() -> None:
-    """
-    Test FloatMother create method with min greater than max.
-    """
-    min_value = FloatMother.negative()
-    max_value = FloatMother.positive()
+    min_value = IntegerMother.negative()
+    max_value = IntegerMother.positive()
 
     with assert_raises(
         expected_exception=ValueError,
@@ -134,152 +105,58 @@ def test_float_mother_min_greater_than_max() -> None:
 
 
 @mark.unit_testing
-def test_float_mother_positive() -> None:
+def test_float_mother_create_method_min_decimals_value() -> None:
     """
-    Test FloatMother positive method.
-    """
-    value = FloatMother.positive()
-
-    assert type(value) is float
-    assert value >= 0
-
-
-@mark.unit_testing
-def test_float_mother_positive_invalid_max_type() -> None:
-    """
-    Test FloatMother positive method with invalid max type.
-    """
-    with assert_raises(
-        expected_exception=TypeError,
-        match='FloatMother max value must be an integer.',
-    ):
-        FloatMother.positive(max=FloatMother.invalid_type(remove_types=(int,)))
-
-
-@mark.unit_testing
-def test_float_mother_positive_max_less_than_one() -> None:
-    """
-    Test FloatMother positive method with max less than 1.
-    """
-    max_value = FloatMother.create(max=0)
-
-    with assert_raises(
-        expected_exception=ValueError,
-        match='FloatMother min value must be less than or equal to max value.',
-    ):
-        FloatMother.positive(max=max_value)
-
-
-@mark.unit_testing
-def test_float_mother_negative() -> None:
-    """
-    Test FloatMother negative method.
-    """
-    value = FloatMother.negative()
-
-    assert type(value) is float
-    assert value <= 0
-
-
-@mark.unit_testing
-def test_float_mother_negative_invalid_min_type() -> None:
-    """
-    Test FloatMother negative method with invalid min type.
-    """
-    with assert_raises(
-        expected_exception=TypeError,
-        match='FloatMother min value must be an integer.',
-    ):
-        FloatMother.negative(min=FloatMother.invalid_type(remove_types=(int,)))
-
-
-@mark.unit_testing
-def test_float_mother_negative_min_greater_than_zero() -> None:
-    """
-    Test FloatMother negative method with min greater than 0.
-    """
-    min_value = FloatMother.create(min=1)
-
-    with assert_raises(
-        expected_exception=ValueError,
-        match='FloatMother min value must be less than or equal to max value.',
-    ):
-        FloatMother.negative(min=min_value)
-
-
-@mark.unit_testing
-def test_float_mother_out_of_range() -> None:
-    """
-    Test FloatMother out_of_range method.
-    """
-    min_value = FloatMother.create(max=0)
-    max_value = FloatMother.create(min=0)
-    range_value = FloatMother.create(min=0)
-    value = FloatMother.out_of_range(min=min_value, max=max_value, range=range_value)
-
-    assert type(value) is float
-    assert value < min_value or value > max_value
-    assert (min_value - range_value <= value <= min_value) or (max_value <= value <= max_value + range_value)
-
-
-@mark.unit_testing
-def test_float_mother_out_of_range_invalid_min_type() -> None:
-    """
-    Test FloatMother out_of_range method with invalid min type.
-    """
-    with assert_raises(
-        expected_exception=TypeError,
-        match='FloatMother min value must be an integer or a float.',
-    ):
-        FloatMother.out_of_range(min=FloatMother.invalid_type(remove_types=(int, float)))
-
-
-@mark.unit_testing
-def test_float_mother_out_of_range_invalid_max_type() -> None:
-    """
-    Test FloatMother out_of_range method with invalid max type.
-    """
-    with assert_raises(
-        expected_exception=TypeError,
-        match='FloatMother max value must be an integer or a float.',
-    ):
-        FloatMother.out_of_range(max=FloatMother.invalid_type(remove_types=(int, float)))
-
-
-@mark.unit_testing
-def test_float_mother_out_of_range_min_greater_than_max() -> None:
-    """
-    Test FloatMother out_of_range method with min greater than max.
-    """
-    min_value = FloatMother.positive()
-    max_value = FloatMother.negative()
-
-    with assert_raises(
-        expected_exception=ValueError,
-        match='FloatMother min value must be less than or equal to max value.',
-    ):
-        FloatMother.out_of_range(min=min_value, max=max_value)
-
-
-@mark.unit_testing
-def test_float_mother_out_of_range_invalid_range_type() -> None:
-    """
-    Test FloatMother out_of_range method with invalid range type.
-    """
-    with assert_raises(
-        expected_exception=TypeError,
-        match='FloatMother range must be an integer or a float.',
-    ):
-        FloatMother.out_of_range(range=FloatMother.invalid_type(remove_types=(int, float)))
-
-
-@mark.unit_testing
-def test_float_mother_out_of_range_negative_range() -> None:
-    """
-    Test FloatMother out_of_range method with negative range.
+    Check that FloatMother create method raises a ValueError when the provided decimals is minimum permitted value.
     """
     with assert_raises(
         expected_exception=ValueError,
-        match='FloatMother range must be a positive value.',
+        match='FloatMother decimals value must be greater than or equal to 0.',
     ):
-        FloatMother.out_of_range(range=FloatMother.negative())
+        FloatMother.create(decimals=-1)
+
+
+@mark.unit_testing
+def test_float_mother_create_method_negative_decimals_random_value() -> None:
+    """
+    Check that FloatMother create method raises a ValueError when the provided decimals is less than zero.
+    """
+    with assert_raises(
+        expected_exception=ValueError,
+        match='FloatMother decimals value must be greater than or equal to 0.',
+    ):
+        FloatMother.create(decimals=IntegerMother.negative())
+
+
+@mark.unit_testing
+def test_float_mother_create_method_max_decimal_value() -> None:
+    """
+    Check that FloatMother create method raises a ValueError when the provided decimals is higher than maximum
+    permitted value.
+    """
+    with assert_raises(
+        expected_exception=ValueError,
+        match='FloatMother decimals value must be less than or equal to 10.',
+    ):
+        FloatMother.create(decimals=11)
+
+
+@mark.unit_testing
+def test_float_mother_create_method_max_decimal_random_value() -> None:
+    """
+    Check that FloatMother create method raises a ValueError when the provided decimals is higher than maximum
+    permitted value.
+    """
+    with assert_raises(
+        expected_exception=ValueError,
+        match='FloatMother decimals value must be less than or equal to 10.',
+    ):
+        FloatMother.create(decimals=IntegerMother.create(min=11))
+
+
+@mark.unit_testing
+def test_float_mother_create_method_invalid_type() -> None:
+    """
+    Check that FloatMother create method raises a TypeError when the provided value is not an integer or a float.
+    """
+    assert type(FloatMother.invalid_type()) is not float

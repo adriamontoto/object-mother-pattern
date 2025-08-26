a04512a1afabe733a03348a6d2a8ad4b91be08d3
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-08-26 08:23:18 +0200

    feat(internet): implement ip address mother
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- object_mother_pattern/mothers/internet/__init__.py
+++ object_mother_pattern/mothers/internet/__init__.py
@@ -1,5 +1,6 @@
 from .aws_cloud_region_mother import AwsCloudRegionMother
 from .domain_mother import DomainMother
+from .ip_address_mother import IpAddressMother
 from .ipv4_address_mother import Ipv4AddressMother
 from .ipv4_network_mother import Ipv4NetworkMother
 from .ipv6_address_mother import Ipv6AddressMother
@@ -11,6 +12,7 @@ from .user_agent_mother import UserAgentMother
 __all__ = (
     'AwsCloudRegionMother',
     'DomainMother',
+    'IpAddressMother',
     'Ipv4AddressMother',
     'Ipv4NetworkMother',
     'Ipv6AddressMother',

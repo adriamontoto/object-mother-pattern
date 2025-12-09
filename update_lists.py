d472e890cd1f60188d8ec0dd3a8124a929b1020c
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-12-09 13:57:58 +0100

    feat(update-lists): if no word list retrieval raise a runtime error
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- update_lists.py
+++ update_lists.py
@@ -35,6 +35,9 @@ def update_aws_cloud_regions() -> None:
     """
     Retrieve AWS cloud regions from the official AWS documentation and update the local AWS regions file..
 
+    Raise:
+        RuntimeError: When unable to retrieve AWS regions from the official documentation.
+
     References:
         AWS Cloud Regions: https://docs.aws.amazon.com/global-infrastructure/latest/regions/aws-regions.html#available-regions
     """
@@ -44,6 +47,8 @@ def update_aws_cloud_regions() -> None:
 
     pattern = r'<tr>\s*<td[^>]*tabindex="-1">(.*?)</td>\s*<td[^>]*tabindex="-1">.*?</td>\s*<td[^>]*tabindex="-1">.*?</td>\s*</tr>'  # noqa: E501
     aws_regions = tuple(region_code.lower() for region_code in findall(pattern=pattern, string=content, flags=DOTALL))
+    if not aws_regions:
+        raise RuntimeError('Failed to retrieve AWS regions from the official documentation.')
 
     path = 'object_mother_pattern/mothers/internet/utils/aws_regions.txt'
     write_if_changed(path=path, lines=aws_regions)
@@ -53,6 +58,9 @@ def update_bip39_words() -> None:
     """
     Retrieve BIP39 words from Bitcoin and update the local BIP39 file.
 
+    Raise:
+        RuntimeError: When unable to retrieve BIP39 words from the official source.
+
     References:
         Bip39 Words: https://github.com/bitcoin/bips/blob/master/bip-0039/english.txt
     """
@@ -61,6 +69,8 @@ def update_bip39_words() -> None:
         lines = response.read().decode('utf-8').splitlines()
 
     bip39_words = tuple(line.strip().lower() for line in lines if line and not line.startswith('#'))
+    if not bip39_words:
+        raise RuntimeError('Failed to retrieve BIP39 words from the official source.')
 
     path = 'object_mother_pattern/mothers/money/cryptocurrencies/utils/bip39_words.txt'
     write_if_changed(path=path, lines=bip39_words)
@@ -70,6 +80,9 @@ def update_tld_domains() -> None:
     """
     Retrieve top-level domains from IANA and update the local TLD file.
 
+    Raise:
+        RuntimeError: When unable to retrieve TLD domains from IANA.
+
     References:
         TLD Domains: https://data.iana.org/TLD/tlds-alpha-by-domain.txt
     """
@@ -78,6 +91,8 @@ def update_tld_domains() -> None:
         lines = response.read().decode('utf-8').splitlines()
 
     tld_domains = tuple(line.strip().lower() for line in lines if line and not line.startswith('#'))
+    if not tld_domains:
+        raise RuntimeError('Failed to retrieve TLD domains from IANA.')
 
     path = 'object_mother_pattern/mothers/internet/utils/tld_domains.txt'
     write_if_changed(path=path, lines=tld_domains)
@@ -87,6 +102,9 @@ def update_word_list() -> None:
     """
     Retrieve a list of 10000 words.
 
+    Raise:
+        RuntimeError: When unable to retrieve the word list from the official source.
+
     References:
         Words: https://www.mit.edu/~ecprice/wordlist.10000
     """
@@ -95,6 +113,8 @@ def update_word_list() -> None:
         lines = response.read().decode('utf-8').splitlines()
 
     words = tuple(line.strip().lower() for line in lines if line)
+    if not words:
+        raise RuntimeError('Failed to retrieve the word list from the official source.')
 
     path = 'object_mother_pattern/mothers/internet/utils/words.txt'
     write_if_changed(path=path, lines=words)
